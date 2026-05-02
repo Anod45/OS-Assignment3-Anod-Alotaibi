c3b3ffd97a2bb2e120f847a28272c2a4878ca313
Anod45 <445052026@STD.PSAU.EDU.SA>
2026-05-02 23:11:09 +1200

    adding Semaphore and import  its package for synchronizaion

--- SchedulerSimulationSync.java
+++ SchedulerSimulationSync.java
@@ -5,7 +5,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 import java.util.Random;
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.locks.ReentrantLock;
+impot java.util.concurrent.Semaphore;
 
 // ANSI Color Codes for enhanced terminal output
 class Colors {
@@ -44,6 +46,7 @@ public static final ReentrantLock lock = new ReentrantLock();
     
     // TODO #2: Add a Semaphore to limit concurrent process execution
     // Example: public static final Semaphore cpuSemaphore = new Semaphore(1);
+    public static final Semaphore cpuSemaphore = new java.util.concurrent.Semaphore(1);
     
     // Method to increment context switch counter
     public static void incrementContextSwitch() {
